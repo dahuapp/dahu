a9f1d6d5af658e4d5781337ad6a5a5c24113b425
Denis Becker <denis.becker@ensimag.imag.fr>
2013-06-06 17:27:37 +0200

    refactoring keyboard driver and proxy

--- editor/src/main/java/io/dahuapp/editor/proxy/KeyboardDriverProxy.java
+++ editor/src/main/java/io/dahuapp/editor/proxy/KeyboardDriverProxy.java
@@ -1,11 +1,67 @@
 package io.dahuapp.editor.proxy;
 
+import io.dahuapp.editor.drivers.KeyboardDriver;
+import io.dahuapp.editor.drivers.KeyboardDriver.KeyboardListener;
+import javafx.application.Platform;
 import javafx.scene.web.WebEngine;
+import netscape.javascript.JSException;
+import netscape.javascript.JSObject;
+import org.jnativehook.keyboard.NativeKeyEvent;
 
 /**
  * Proxy for the keyboard driver.
  */
 public class KeyboardDriverProxy implements Proxy {
+    
+    /**
+     * Keyboard listener implementing the interface of Keyboard driver.
+     */
+    public class KeyboardListener implements KeyboardDriver.KeyboardListener {
+        
+        /**
+         * Name of the callback ('dahuapp.editor.-callback-').
+         */
+        private final String callback;
+        
+        /**
+         * Constructor.
+         * @param callback Callback to call if keyCode is pressed.
+         */
+        public KeyboardListener(String callback) {
+            this.callback = callback;
+        }
+
+        @Override
+        public void keyTyped(int keyCode) {
+            // ignored
+        }
+
+        @Override
+        public void keyPressed(int keyCode) {
+            // ignored
+        }
+
+        @Override
+        public void keyReleased(int keyCode) {
+            JSObject engine = (JSObject)webEngine.executeScript("window.dahuapp.engine");
+            engine.call(callback, keyCode);
+        }
+        
+        /**
+         * Equals method is redefined to know when two listeners are the
+         * same, in order to remove them from the listener list.
+         * @param o Object to compare this with.
+         * @return True if this is equals to the specified object.
+         */
+        @Override
+        public boolean equals(Object o) {
+            if (o instanceof KeyboardListener) {
+                KeyboardListener k = (KeyboardListener)o;
+                return k.callback.equals(this.callback);
+            }
+            return false;
+        }
+    }
 
     /**
      * Driver associated with this proxy.
@@ -25,11 +81,43 @@ public class KeyboardDriverProxy implements Proxy {
         this.webEngine = webEngine;
     }
     
+    /**
+     * Adds a listener (javascript function) to the keyboard driver.
+     * @param listener Javascript function called when a key is pressed.
+     */
+    public void addKeyListener(JSObject listener) throws JSException {
+        final String functionName = listener.getMember("name").toString();
+        switch (functionName) {
+            case "undefined":
+                throw new JSException("Callback function cannot be anonymous.");
+            case "":
+                throw new JSException("Callback function cannot be anonymous.");
+            default:
+                driver.addKeyListener(new KeyboardListener(functionName));
+        }
+    }
+    
+    /**
+     * Removes a listener (javascript function) to the keyboard driver.
+     * @param listener Javascript function called when a key is pressed.
+     */
+    public void removeKeyListener(JSObject listener) throws JSException {
+        final String functionName = listener.getMember("name").toString();
+        switch (functionName) {
+            case "undefined":
+                throw new JSException("Callback function cannot be anonymous.");
+            case "":
+                throw new JSException("Callback function cannot be anonymous.");
+            default:
+                driver.removeKeyListener(new KeyboardListener(functionName));
+        }
+    }
+    
     @Override
     public void onLoad() {
         driver.onLoad();
     }
-
+    
     @Override
     public void onStop() {
         driver.onStop();
