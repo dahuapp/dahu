4ab058669fcd2f158473600e43bc580377e6e9ef
Mathieu Gaudron <mathieu.gaudron@phelma.grenoble-inp.fr>
2013-06-07 14:18:26 +0200

    modified dahuapp.editor.js
    Added features in the open-project button : allows to show all the slides of a project that we opened.

--- editor/src/main/resources/io/dahuapp/editor/app/dahuapp.editor.js
+++ editor/src/main/resources/io/dahuapp/editor/app/dahuapp.editor.js
@@ -116,7 +116,7 @@ var dahuapp = (function(dahuapp, $) {
          * in the application window.
          */
         self.init = function init() {
-            
+
             $('#capture-mode').click(function() {
                 if (initProject) {
                     switchCaptureMode();
@@ -132,10 +132,24 @@ var dahuapp = (function(dahuapp, $) {
             $('#open-project').click(function() {
                 if (!captureMode) {
                     var driver = dahuapp.drivers.fileSystem;
-                    projectDir = driver.askForProjectDir();
+                    // projectDir = driver.askForProjectDir();
                     if (projectDir) {
                         var stringJson = driver.readFile(projectDir + driver.getSeparator() + jsonFileName);
+                        var slideList;
+                        var i = 0;
                         dahuapp.editor.json.loadJson(stringJson);
+                        slideList = dahuapp.editor.json.getSlideList();
+                        while (slideList[i]) {
+                            var img = slideList[i];
+                            $('#image-list').append($(document.createElement('li'))
+                                    .attr({'class': 'span2 offset'})
+                                    .append($(document.createElement('a'))
+                                    .attr({'class': 'thumbnail'})
+                                    .append($(document.createElement('img'))
+                                    .attr({'src': img, 'alt': img}))));
+                            i++;
+                        }
+
                         initProject = true;
                     }
                 }
@@ -143,7 +157,7 @@ var dahuapp = (function(dahuapp, $) {
             $('#new-project').click(function() {
                 if (!captureMode) {
                     dahuapp.drivers.dialog.showMessage("Info",
-                        "The project was successfully created.");
+                            "The project was successfully created.");
                     dahuapp.editor.json.createPresentation();
                     initProject = true;
                     dahuapp.editor.json.createPresentation();
@@ -151,7 +165,7 @@ var dahuapp = (function(dahuapp, $) {
                 }
             });
         };
-        
+
         /**
          * 
          * @param {type} args
