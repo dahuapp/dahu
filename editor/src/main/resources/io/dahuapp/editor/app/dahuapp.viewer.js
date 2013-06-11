97a2936be634df32a54799645e650a8d46d85aca
Jérémy Rasser <jeremy.rasser@gmail.com>
2013-06-11 09:31:48 +0200

    Modifying the JSON loader in dahuapp.viewer.js
    Creation of viewer module, with skeleton and next and previous events.

--- editor/src/main/resources/io/dahuapp/editor/app/dahuapp.viewer.js
+++ editor/src/main/resources/io/dahuapp/editor/app/dahuapp.viewer.js
@@ -14,43 +14,60 @@ var dahuapp = (function(dahuapp, $) {
         var self = {};
 
         /**
-         * The screenshots will have to be described with CSS "screen" class.
-         * Variable "max aims to count the screenshots taken for the presentation.
+         * Ajax request to load the JSON document. 
          */
+        var json = (function() {
+            var json = null;
+            $.ajax({
+                'async': false,
+                'global': false,
+                'url': 'presentation.dahu',
+                'dataType': "json",
+                'success': function(data) {
+                    json = data;
+                }
+            });
+            return json;
+        })();
 
-        var max = $(".screen").size();
+        /*
+         * Variable storing the total number of slides.
+         */
+        var max = JSON.stringify(json.metaData.nbSlide);
 
-        /**
+        /*
          * "currentImage" is the main image being visualised.
          */
-
         var currentImage = 0;
 
-
-        /**
+        /*
          *At the beginning, the visible image is the first one of the presentation
          */
-
         self.init = function init() {
             for (var i = 0; i < max; i++) {
                 $("#" + i).hide();
             }
             $("#" + currentImage).show();
 
-            /**
+            /*
              * A click on the "next" button publishes a nextSlide event
              */
             $("#next").click(function() {
-                events.nextSlide.publish();
+                if (currentImage < max) {
+                    currentImage++;
+                    events.nextSlide.publish(currentImage);
 
-
+                }
             });
 
-            /**
+            /*
              * A click on the "previous" button publishes a previousSlide event
              */
             $("#previous").click(function() {
-                events.previousSlide.publish();
+                if (currentImage > 0) {
+                    currentImage--;
+                    events.previousSlide.publish(currentImage);
+                }
             });
         }
 
@@ -59,7 +76,6 @@ var dahuapp = (function(dahuapp, $) {
     })();
 
     dahuapp.viewer = viewer;
-    dahuapp.viewer.init();
 
     return dahuapp;
 })(dahuapp || {}, jQuery);
