7f5be179500428853e937e78002c6755e443fefc
clement collignon <clement.collignon@phelma.grenoble-inp.fr>
2013-06-16 21:20:36 +0200

    Modification for events management
    Change all callbacks due to the new format of the json viewer.

--- editor/src/main/resources/io/dahuapp/editor/app/dahuapp.viewer.js
+++ editor/src/main/resources/io/dahuapp/editor/app/dahuapp.viewer.js
@@ -30,7 +30,8 @@ var dahuapp = (function(dahuapp, $) {
                     return {
                         publish: callbacks.fire,
                         subscribe: callbacks.add,
-                        unsubscribe: callbacks.remove
+                        unsubscribe: callbacks.remove,
+                        unsubscribeAll: callbacks.empty
                     };
                 };
                 /*
@@ -55,39 +56,38 @@ var dahuapp = (function(dahuapp, $) {
             /*
              * Variables used like index for methodes subscribed.
              */
-            var currentSlide = 0;
-            var lastSlide = 0;
             var currentAction = 0;
-
+            var nextAction = 0; 
+            var allActionFinish = 0;
             /*
              * Function used when an "onNextEvent" event is caught.
              */
             var onNextEventHandler = function() {
-                while (json.data[currentSlide]) {
-                    if (currentAction < 0) {
-                        currentAction = 0;
-                    }
-                    while (json.data[currentSlide].action[currentAction]) {
-                        if (json.data[currentSlide].action[currentAction].trigger === 'onClick') {
-                            var mouseLocation = previousMouseLocation(currentSlide, currentAction);
-                            $(selector + " ." + "mouse-cursor").animate({'top': mouseLocation.ord * 100 + "\%",
-                                'left': mouseLocation.abs * 100 + "\%"}, 0);
-                            launch(json.data[currentSlide].action[currentAction++]);
+                $(selector + " .object-list").children().stop(true, true);
+                if(json.action[nextAction]) {
+                    launch(json.action[nextAction++]);
+                    currentAction = nextAction;
+                } else if (nextAction < 0) {
+                    currentAction = 0;
+                    nextAction = 0;
+                }
+                while (json.action[currentAction]) {
+                    switch(json.action[currentAction].trigger){
+                        case 'onClick':
+                            nextAction = currentAction;
                             return;
-                        }
-                        currentAction++;
+                        case 'withPrevious':
+                            events.onActionStart.subscribe(json.data[currentAction].execute); 
+                            break;
+                        case 'afterPrevious':
+                            events.onAllActionFinish.subscribe(json.data[currentAction].execute);
+                            while (json.action[currentAction].trigger !== 'onClick') {
+                                currentAction++;
+                            }
+                            nextAction = currentAction;
+                        break;
                     }
-                    if (currentSlide < json.data.length - 1) {
-                        lastSlide = currentSlide;
-                        currentSlide++;
-                        currentAction = 0;
-                        actualise();
-                    } else {
-                        var mouseLocation = previousMouseLocation(currentSlide, currentAction);
-                        $(selector + " ." + "mouse-cursor").animate({'top': mouseLocation.ord * 100 + "\%",
-                            'left': mouseLocation.abs * 100 + "\%"}, 0);
-                        return;
-                    }
+                    currentAction++;
                 }
             };
 
@@ -95,49 +95,32 @@ var dahuapp = (function(dahuapp, $) {
              * Function used when an "onPreviousEvent" event is caught.
              */
             var onPreviousEventHandler = function() {
-                currentAction--;
-                while (json.data[currentSlide]) {
-                    while (json.data[currentSlide].action[currentAction]) {
-                        if (json.data[currentSlide].action[currentAction].trigger === 'onClick') {
-                            var mouseLocation = previousMouseLocation(currentSlide, currentAction);
-                            $(selector + " ." + "mouse-cursor").animate({'top': mouseLocation.ord * 100 + "\%",
-                                'left': mouseLocation.abs * 100 + "\%"}, 0);
-                            return;
-                        }
-                        currentAction--;
-                    }
-                    if (currentSlide > 0) {
-                        lastSlide = currentSlide;
-                        currentSlide--;
-                        actualise();
-                        currentAction = json.data[currentSlide].indexAction - 1;
-                    } else {
-                        var mouseLocation = previousMouseLocation(currentSlide, currentAction);
-                        $(selector + " ." + "mouse-cursor").animate({'top': mouseLocation.ord * 100 + "\%",
-                            'left': mouseLocation.abs * 100 + "\%"}, 0);
-                        return;
-                    }
+                $(selector + " .object-list").children().stop(true, true);
+                while(json.action[--currentAction].trigger !== 'onClick') {
+                    launch(json.action[currentAction]);
                 }
+                nextAction = currentAction;
             };
 
             /*
              * Function used when an "onActionOverEvent" event is caught.
              */
-            var onActionOverEventHandler = function() {
-                if (json.data[currentSlide].action[currentAction]) {
-                    if (json.data[currentSlide].action[currentAction].trigger === 'afterPrevious') {
-                        launch(json.data[currentSlide].action[currentAction++]);
-                    }
-                } else {
-                    if (currentSlide < json.data.length - 1 && json.data[currentSlide + 1].action[0]) {
-                        if (json.data[currentSlide + 1].action[0].trigger === 'afterPrevious') {
-                            lastSlide = currentSlide;
-                            currentSlide++;
-                            actualise();
-                            currentAction = 0;
-                            launch(json.data[currentSlide].action[currentAction++]);
-                        }
-                    }
+            var onActionOverEventHandler = function() { 
+                allActionFinish--;
+                if (allActionFinish === 0) {
+                    events.onAllActionFinish.publish();
+                    events.onActionStart
+                    events.onAllActionFinish
+                }
+                switch (json.action[currentAction].trigger) {
+                    case 'onClick':
+                        return;
+                    case 'withPrevious':
+                        events.onActionStart.subscribe(json.data[currentAction].execute); 
+                        break;
+                    case 'afterPrevious':
+                        events.onAllActionFinish.subscribe(json.data[currentAction].execute);
+                        break;
                 }
             };
 
@@ -145,92 +128,18 @@ var dahuapp = (function(dahuapp, $) {
              * Function used when an "onActionStartEvent" event is caught.
              */
             var onActionStartEventHandler = function() {
-                if (json.data[currentSlide].action[currentAction]) {
-                    if (json.data[currentSlide].action[currentAction].trigger === 'withPrevious') {
-                        launch(json.data[currentSlide].action[currentAction++]);
-                    }
-                } else {
-                    if (currentSlide < json.data.length - 1 && json.data[currentSlide + 1].action[0]) {
-                        if (json.data[currentSlide + 1].action[0].trigger === 'withPrevious') {
-                            lastSlide = currentSlide;
-                            currentSlide++;
-                            currentAction = 0;
-                            launch(json.data[currentSlide].action[currentAction++]);
-                        }
-                    }
-                }
-            };
-
-            /*
-             * Function used to actualise the background when there is a new slide.
-             */
-            var actualise = function() {
-                $(selector + " ." + json.data[lastSlide].object[0].id).hide();
-                $(selector + " ." + json.data[currentSlide].object[0].id).show();
+                allActionFinish++;
             };
 
             /*
              * Function used to realise actions.
              */
             var launch = function(action) {
-                var NextActionWithPrevious = hasNextActionWithPrevious(currentSlide, currentAction - 1);
-                events.onActionStart.publish();
-                var target = selector + ' .' + action.target;
-                var animation = eval('(' + action.execute + ')');
-                animation(target, action.finalAbs, action.finalOrd);
-                if (!NextActionWithPrevious) {
-                    events.onActionOver.publish();
-                }
+                action.execute(json.metaData.image.width, json.metaData.image.Height);
             };
-
-            /*
-             * Check the trigger of the next action.
-             * @param int idSlide 
-             * @param int idAction
-             * @returns true if the next action's trigger is "withPrevious"
-             */
-            var hasNextActionWithPrevious = function(idSlide, idAction) {
-                if (json.data[idSlide].action[idAction]) {
-                    if (json.data[idSlide].action[idAction].trigger === 'withPrevious') {
-                        return true;
-                    }
-                } else {
-                    if (idSlide < json.data.length - 1) {
-                        idSlide++;
-                        idAction = 0;
-                    }
-                    if (json.data[idSlide].action[idAction]
-                            && json.data[idSlide].action[idAction].trigger === 'withPrevious') {
-                        return true;
-                    }
-                }
-                return false;
-            };
-
-            /*
-             * Looks for the previous mouse location.
-             * @param int idSlide 
-             * @param int idAction
-             * @returns the mouse location before the current action.
-             */
-            var previousMouseLocation = function(idSlide, idAction) {
-                var tempAction = 0;
-                var mouseLocation = {};
-                if (idAction === 0 && idSlide > 0) {
-                    idSlide--;
-                    idAction = json.data[idSlide].action.length;
-                } else if (idAction === 0 && idSlide <= 0) {
-                    mouseLocation.abs = json.data[0].action[0].finalAbs;
-                    mouseLocation.ord = json.data[0].action[0].finalOrd;
-                }
-                while (tempAction < idAction) {
-                    if (json.data[idSlide].action[tempAction].target === "mouse-cursor") {
-                        mouseLocation.abs = json.data[idSlide].action[tempAction].finalAbs;
-                        mouseLocation.ord = json.data[idSlide].action[tempAction].finalOrd;
-                    }
-                    tempAction++;
-                }
-                return mouseLocation;
+            
+            var launchReverse = function(action) {
+                action.executeReverse(json.metaData.image.width, json.metaData.image.Height);
             };
 
             /* Public API */
@@ -249,6 +158,10 @@ var dahuapp = (function(dahuapp, $) {
 
             this.start = function() {
 
+                for (var i = 0; i < json.action.length; i++) {
+                    json.action[i].execute = eval('(' + json.action[i].execute + ')');
+                    json.action[i].executeReverse = eval('(' + json.action[i].executeReverse + ')');
+                }
                 /*
                  * Subscription of methods to their events.
                  */
@@ -256,6 +169,7 @@ var dahuapp = (function(dahuapp, $) {
                 events.onPrevious.subscribe(onPreviousEventHandler);
                 events.onActionOver.subscribe(onActionOverEventHandler);
                 events.onActionStart.subscribe(onActionStartEventHandler);
+               
                 /*
                  * Variable storing the total number of slides.
                  */
@@ -264,9 +178,8 @@ var dahuapp = (function(dahuapp, $) {
                 /*
                  *At the beginning, the visible image is the first one of the presentation
                  */
-                for (var i = 0; i < max; i++) {
-                    $(selector + " .s" + i + "-o0").hide();
-                }
+                $(selector + " .image-list").hide();
+
                 $(selector + " ." + json.data[0].object[0].id).show();
 
                 $(selector + " ." + "mouse-cursor").css({'top': json.data[0].action[0].finalOrd * 100 + "\%",
