8eb4ce6790a64a8cbe57f11b35a509c893758ad3
Denis Becker <denis.becker@ensimag.imag.fr>
2013-10-02 17:40:23 +0200

    Platform-compatibility improved
    On some linux platforms (for example on my Ubuntu 13.04 32 bits) the
    java.awt.Desktop API is not supported and causes that the application
    crashes instantaneously when it is run.
    The java.awt.Desktop class is used to have access to the default web
    browser of the platform.
    
    To avoid this problem, I added
    a java library (BrowserLauncher2) which is an opensource API for Java
    made for web browser opening. As it is an external library, it is only
    used when java.awt.Desktop is not supported, so the Desktop API has
    the priority when it works.

--- editor/src/main/java/io/dahuapp/editor/proxy/BrowserProxy.java
+++ editor/src/main/java/io/dahuapp/editor/proxy/BrowserProxy.java
@@ -1,8 +1,10 @@
 package io.dahuapp.editor.proxy;
 
+import edu.stanford.ejalbert.BrowserLauncher;
+import edu.stanford.ejalbert.exception.BrowserLaunchingInitializingException;
+import edu.stanford.ejalbert.exception.UnsupportedOperatingSystemException;
 import java.awt.Desktop;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import javafx.application.Platform;
@@ -16,54 +18,80 @@ public class BrowserProxy implements Proxy {
      * Allows this proxy to open the default browser.
      */
     private Desktop desktop;
-    
+    /**
+     * Alternative way to open a browser (Desktop is not supported on many linux
+     * platforms).
+     */
+    private BrowserLauncher browserLauncher;
+
     /**
      * Runs the preview (opens the default web browser).
+     *
      * @param htmlFile The absolute path to the html file to display.
      */
     public void runPreview(String htmlFile) {
-        try {
-            /* 
-             * The 'replaceAll' is a fix for windows.
-             * The URI seems not to support strings with '\' so we replace all
-             * of them by '/' otherwise a malformed URI exception is thrown.
-             */
-            final URI u = new URI("file://" + htmlFile.replaceAll("\\\\", "/"));
-            Platform.runLater(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        desktop.browse(u);
-                    } catch (IOException  e) {
-                        LoggerProxy.severe("Browser couldn't be opened.", e);
-                    }
-                }
-            });
-        } catch (URISyntaxException e) {
-            LoggerProxy.severe("Error in URI Syntax", e);
-        }
+        /*
+         * The 'replaceAll' is a fix for windows.
+         * The URI seems not to support strings with '\' so we replace all
+         * of them by '/' otherwise a malformed URI exception is thrown.
+         */
+        final String urlString = "file://" + htmlFile.replaceAll("\\\\", "/");
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                openWebBrowser(urlString);
+            }
+        });
     }
-    
+
     /**
      * Open the default web browser and open the specified URL.
+     *
      * @param url Page to display on the browser.
      */
     public void openURL(final String url) {
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
-                try {
-                    desktop.browse(new URL(url).toURI());
-                } catch (URISyntaxException | IOException e) {
-                    LoggerProxy.severe("Browser couldn't be opened.", e);
-                }
+                openWebBrowser(url);
             }
         });
     }
-    
+
+    /**
+     * This method opens a web browser and loads the given URL into it.
+     * Depending on the platform, a different method can be used to run the web
+     * browser.
+     *
+     * @param url The URL to open.
+     */
+    private void openWebBrowser(String url) {
+        if (desktop != null) {
+            try {
+                desktop.browse(new URL(url).toURI());
+            } catch (URISyntaxException | IOException e) {
+                LoggerProxy.severe("Browser couldn't be opened.", e);
+            }
+        } else if (browserLauncher != null) {
+            browserLauncher.openURLinBrowser(url);
+        } else {
+            // Maybe add a message popup here
+            LoggerProxy.severe("Browser driver not supported.");
+        }
+    }
+
     @Override
     public void onLoad() {
-        desktop = Desktop.getDesktop();
+        if (Desktop.isDesktopSupported()) {
+            desktop = Desktop.getDesktop();
+        } else {
+            try {
+                browserLauncher = new BrowserLauncher();
+            } catch (BrowserLaunchingInitializingException
+                    | UnsupportedOperatingSystemException e) {
+                LoggerProxy.severe("BrowserLauncher couldn't be opened.", e);
+            }
+        }
     }
 
     @Override
