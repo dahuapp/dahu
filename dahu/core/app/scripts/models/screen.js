aca68920793145e86ce69aea6b7ce7b7a9c82223
Remi Barraquand <dev@remibarraquand.com>
2014-05-19 23:01:57 +0200

    adding objects collection to screen model

--- dahu/core/app/scripts/models/screen.js
+++ dahu/core/app/scripts/models/screen.js
@@ -1,15 +1,17 @@
 define([
     'underscore',
     'backbone',
-    'uuid'
-], function(_, Backbone, UUID){
+    'uuid',
+    'collections/objects'
+], function(_, Backbone, UUID, ObjectCollection){
 
     /**
      * Base Screen model.
      */
     var ScreenModel = Backbone.Model.extend({
         defaults: {
-            id: UUID.v4()
+            id: UUID.v4(),
+            objects: new ObjectCollection()
         }
     });
 
