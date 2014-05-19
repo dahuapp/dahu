8939e236387133ad5b06fc3d985084f49f90b573
Remi Barraquand <dev@remibarraquand.com>
2014-05-19 22:59:13 +0200

    create metadata model

---
+++ dahu/core/app/scripts/models/metadata.js
@@ -0,0 +1,23 @@
+/**
+ * Created by barraq on 19/05/14.
+ */
+
+define([
+    'underscore',
+    'backbone'
+], function(_, Backbone){
+
+    /**
+     * Base object model.
+     */
+    var MetadataModel = Backbone.Model.extend({
+        defaults: {
+            title: _.template('Dahu Screencast - <%= date %>', date=Date.now()),
+            author: undefined,
+            created_at: Date.now(),
+            modified_at: Date.now()
+        }
+    });
+
+    return MetadataModel;
+});
