62a3610608c3d0148f6e7af0ce898ac8cd16e589
Remi Barraquand <dev@remibarraquand.com>
2014-05-21 21:45:21 +0200

    replacing Backbone.event by Backbone.Wreqr event

--- dahu/core/app/scripts/modules/events.js
+++ dahu/core/app/scripts/modules/events.js
@@ -2,7 +2,6 @@
  * Created by barraq on 04/05/14.
  */
 
-define(['backbone', 'underscore'], function (Backbone, _) {
-    var events = _.extend({}, Backbone.Events);
-    return events;
+define(['backbone.wreqr'], function (Wreqr) {
+    return new Wreqr.EventAggregator();
 });
