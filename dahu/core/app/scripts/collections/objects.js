0d66b91b074bd21269a1ffce48edf44b609f763b
Nabil BENABBOU <nabil.benabbou@ensimag.grenoble-inp.fr>
2014-05-28 20:02:03 +0200

    create specific object models depending on its type

--- dahu/core/app/scripts/collections/objects.js
+++ dahu/core/app/scripts/collections/objects.js
@@ -5,14 +5,24 @@
 define([
     'underscore',
     'backbone',
-    'models/object'
-], function(_, Backbone, ObjectModel){
+    'models/object',
+    'models/objects/background'
+], function(_, Backbone, ObjectModel, BackgroundModel){
 
     /**
      * Base *Object* collection.
      */
     var ObjectCollection = Backbone.Collection.extend({
-        model: ObjectModel
+
+        // The model depends on the type of the object
+        model: function(attrs, options) {
+            switch(attrs.type) {
+                case "background":
+                    return new BackgroundModel(attrs, options);
+                default:
+                    return new ObjectModel(attrs, options);
+            }
+        }
     });
 
     return ObjectCollection;
