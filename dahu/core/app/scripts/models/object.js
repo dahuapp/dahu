115f92f5cc7980e72d948dd2d243d5d14fdb9666
Remi Barraquand <dev@remibarraquand.com>
2014-05-29 14:19:18 +0200

    better handling object model defaults values

--- dahu/core/app/scripts/models/object.js
+++ dahu/core/app/scripts/models/object.js
@@ -8,12 +8,10 @@ define([
      * Base object model.
      */
     var ObjectModel = Backbone.Model.extend({
-        defaults: {
-            id: null
-        },
-
-        initialize: function () {
-            this.set('id', this.get('id') || UUID.v4());
+        defaults: function() {
+            return {
+                id: UUID.v4()
+            }
         }
     });
 
