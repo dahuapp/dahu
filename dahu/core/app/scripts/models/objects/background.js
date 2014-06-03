7927fd868115e98e6bd997b9c129c56aa68724ab
Aurélien Dufour <Aurlien.Dufour@ensimag.grenoble-inp.fr>
2014-06-03 23:57:42 +0200

    create background model

---
+++ dahu/core/app/scripts/models/objects/background.js
@@ -0,0 +1,23 @@
+/**
+ * Created by dufourau on 5/28/14.
+ */
+
+define([
+    'underscore',
+    'backbone',
+    'models/object'
+], function(_, Backbone, ObjectModel){
+
+    /**
+     *  Model of background object
+     */
+    var BackgroundModel = ObjectModel.extend({
+        defaults: {
+            type: 'background',
+            img: null
+        },
+
+    });
+
+    return BackgroundModel;
+});
