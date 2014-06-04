f404a475f7a45f88a447bb8b4853e9289a8ebb2f
Nabil BENABBOU <nabil.benabbou@ensimag.grenoble-inp.fr>
2014-06-04 21:15:55 +0200

    relate the keyboard kernel module to the kernel : javascript

--- dahu/core/app/scripts/modules/kernel/SCI.js
+++ dahu/core/app/scripts/modules/kernel/SCI.js
@@ -16,8 +16,9 @@ define([
     // kernel modules
     // @todo we might do some optimization (check with @barraq)
     'modules/kernel/console',
-    'modules/kernel/filesystem'
-], function(_, Backbone, Console, Filesystem) {
+    'modules/kernel/filesystem',
+    'modules/kernel/Keyboard'
+], function(_, Backbone, Console, Filesystem, Keyboard) {
 
     var self = {};
 
@@ -45,6 +46,7 @@ define([
             // start the kernel
             kernel.start();
             expose('filesystem', Filesystem);
+            expose('keyboard', Keyboard);
         } else {
             // we do not have an existing kernel
             // this case happen when we are running
@@ -52,6 +54,7 @@ define([
             // modules loaded here or just for debugging purpose
             // you can load what you want!
             expose('filesystem', Filesystem); // fallback Filesystem
+            expose('keyboard', Keyboard); // fallback Keyboard
         }
 
         // console is mandatory
