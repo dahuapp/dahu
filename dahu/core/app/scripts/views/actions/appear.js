abec0a79840f9bdc1b1e599dba8d0de8c5440e4e
Nabil BENABBOU <nabil.benabbou@ensimag.grenoble-inp.fr>
2014-06-14 21:10:12 +0200

    editable appear view

--- dahu/core/app/scripts/views/actions/appear.js
+++ dahu/core/app/scripts/views/actions/appear.js
@@ -12,7 +12,61 @@ define([
      * Appear action view
      */
     var appearView = Marionette.ItemView.extend({
-        template: Handlebars.default.compile(Actions_appear_tpl)
+        template: Handlebars.default.compile(Actions_appear_tpl),
+
+        templateHelpers: {
+            isMyTrigger: function(trigger) {
+                if (this.trigger == trigger) {
+                    return "selected";
+                }
+            },
+            hasSpeedAndDelayAfter: function() {
+                // we show the speed and delayAfter inputs only when the
+                // trigger is afterPrevious
+                if (this.trigger != "afterPrevious") {
+                    return "hidden";
+                }
+            }
+        },
+
+        ui: {
+            triggerSelector: "select[id=appearTrigger]",
+            speedInput: "input[id=appearSpeed]",
+            durationInput: "input[id=appearDuration]",
+            delayAfterInput: "input[id=appearDelayAfter]"
+        },
+
+        events: {
+            "click .editAppear" : "modify"
+        },
+
+        modelEvents: {
+            'change': 'fieldsChanged'
+        },
+
+        fieldsChanged: function() {
+            this.render();
+        },
+
+        modify: function() {
+            var triggerVal = this.ui.triggerSelector.val();
+            var durationVal = this.ui.durationInput.val();
+            var speedVal = this.ui.speedInput.val();
+            var delayAfterVal = this.ui.delayAfterInput.val();
+
+            if (this.trigger != triggerVal) {
+                this.model.set('trigger', triggerVal);
+            }
+            if (this.duration != durationVal) {
+                this.model.set('duration', parseInt(durationVal));
+            }
+            if (this.speed != speedVal) {
+                this.model.set('speed', parseFloat(speedVal));
+            }
+            if (this.delayAfter != delayAfterVal) {
+                this.model.set('delayAfter', parseInt(delayAfterVal));
+            }
+        }
     });
 
     return appearView;
