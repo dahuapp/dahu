f3bd0897487959ea1382e57fb6a92f6dfe35dc73
Remi Barraquand <dev@remibarraquand.com>
2014-07-28 19:47:12 +0200

    add support for changing verbosity in CLI

--- dahu/cli/src/main/resources/io/dahuapp/cli/CLI.js
+++ dahu/cli/src/main/resources/io/dahuapp/cli/CLI.js
@@ -1,9 +1,19 @@
+(function(context) {
+
 // start the core
 dahubridge.start();
 
 // get arguments
-var arguments = CLI.arguments;
+var args = CLI.arguments;
 
-if (arguments['generate']) {
-    dahubridge.generate(arguments['<project>'])
+if (args['--quiet']) {
+    kernel.console.setLevel('OFF');
+} else if (args['--verbose']) {
+    kernel.console.setLevel(args['--verbosity'])
 }
+
+if (args['generate']) {
+    dahubridge.generate(args['<project>'])
+}
+
+})(this);
