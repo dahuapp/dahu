30e2d1f95e45807b56fd9d2d8668f8ed8a113cd7
Rémy Rey <remy.rey@ensimag.grenoble-inp.fr>
2015-06-01 10:48:54 +0200

    Introduce animation edition
    Display the different actions with an alternative menu when clicked.
    User can select the parameters via select lists and number inputs.
    
    Add :
    - Full templating with a central template 'action' and partials for each
    specific type of animations 'move', 'appear', 'disappear'.
    - inheritance between the views to control the templating and minimize
    the code redundancie
    - new set of events triggered by user actions in 'action' view. Associated
    callbacks for (re)rendering and model updates.
    - attributes in actions models to match the parameters used in preview

--- dahu/core/app/scripts/views/workspace/actions/disappear.js
+++ dahu/core/app/scripts/views/workspace/actions/disappear.js
@@ -5,19 +5,29 @@
 define([
     'handlebars',
     'backbone.marionette',
+    // views
+    'views/workspace/actions/action',
     // templates
+    'text!templates/views/workspace/actions/action.html',
     'text!templates/views/workspace/actions/disappear.html'
 ], function(
     Handlebars,
     Marionette,
+    // views
+    ActionView,
     // templates
+    actionTemplate,
     disappearTemplate
 ) {
     
     /**
      * Disappear action view
      */
-    return Marionette.ItemView.extend({
-        template: Handlebars.default.compile(disappearTemplate)
+    return ActionView.extend({
+
+        initialize: function (options) {
+            ActionView.prototype.initialize.call(this, options);
+            Handlebars.registerPartial("disappearParametersPartial", Handlebars.default.compile(disappearTemplate));
+        }
     });
 });
--- dahu/core/app/scripts/views/workspace/actions/move.js
+++ dahu/core/app/scripts/views/workspace/actions/move.js
@@ -5,19 +5,46 @@
 define([
     'handlebars',
     'backbone.marionette',
+    // views
+    'views/workspace/actions/action',
     // templates
+    'text!templates/views/workspace/actions/action.html',
     'text!templates/views/workspace/actions/move.html'
 ], function(
     Handlebars,
     Marionette,
+    // views
+    ActionView,
     // templates
+    actionTemplate,
     moveTemplate
 ) {
     
     /**
      * Move action view
      */
-    return Marionette.ItemView.extend({
-        template: Handlebars.default.compile(moveTemplate)
+    return ActionView.extend({
+
+        initialize: function (options) {
+            ActionView.prototype.initialize.call(this, options);
+            Handlebars.registerPartial("moveParametersPartial", Handlebars.default.compile(moveTemplate));
+        },
+
+        triggers: function () {
+            return _.extend({}, ActionView.prototype.triggers,{
+                "change #trXChoice": "move:trx:change",
+                "change #trYChoice": "move:try:change"
+            });
+        },
+
+        hasExtraParameters: true,
+
+        onMoveTrxChange: function () {
+            this.model.attributes.trX = $("#trXChoice").val();
+        },
+
+        onMoveTryChange: function () {
+            this.model.attributes.trY = $("#trYChoice").val();
+        }
     });
 });
