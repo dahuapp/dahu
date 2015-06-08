05f6c8d41d5bd3fc3e14d0ea8268c99ee5cff05e
Maxime Pia <maxime.pia@neuf.fr>
2015-06-08 15:03:43 +0200

    Modify screencast template to take actions into account
    The screencast template now uses the deck.animator plugin to
    play the animations associated to every screen in the browser.

--- dahu/core/app/scripts/modules/helpers/template.js
+++ dahu/core/app/scripts/modules/helpers/template.js
@@ -59,6 +59,28 @@ define([
             // return URL
             return options.type + '://' + Paths.join(parts);
         });
+        
+        /**
+         * Returns a string matching the JSON object of 'element'.
+         *
+         * @param element Element to turn into a String
+         * @param indentNb Number of spaces in an indentation
+         * @param initiaIndent Number of indentations to add at the beginning of each line
+         * @returns {string} a formatted JSON object as a String.
+         */
+        Handlebars.default.registerHelper('dumpJSON', function (element, indentNb, initialIndent) {
+            var res = JSON.stringify(element, null, indentNb);
+            res = res.replace(/^/gm, Array(initialIndent*indentNb+1).join(' '));
+            return res;
+        });
+
+        /**
+         * Returns a string without any dashes, since they are unauthorized
+         * in variable names in JavaScript.
+         */
+        Handlebars.default.registerHelper('removeDashes', function (str) {
+            return str.replace(/-/g, "");
+        });
     }
 
     return {
